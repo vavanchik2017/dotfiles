ae73423305112079b175eba92aef2149adea9e34
noirhat <noirhat@noirhat.com>
2017-06-11 01:30:09 +0300

    add awesome wm config

---
+++ .config/awesome/themes/main/theme.lua
@@ -0,0 +1,83 @@
+theme												= {}
+
+-- Variables
+color_red										= "#FF0000"
+color_yellow									= "#FFAF00"
+color_white										= "#EEEEEC"
+color_black										= "#121212"
+font_terminus									= "xos4 Terminus Regular 8"
+themes_dir										= os.getenv("HOME") .. "/.config/awesome/themes/main"
+theme.wallpaper								= themes_dir .. "/background.png"
+
+-- Main
+theme.font										= font_terminus
+theme.taglist_font							= font_terminus
+theme.fg_normal								= color_white
+theme.fg_focus									= color_yellow
+theme.fg_urgent								= color_red
+theme.bg_normal								= color_black
+theme.bg_focus									= color_black
+theme.bg_urgent								= color_black
+theme.border_width							= "0"
+theme.border_normal							= color_black
+theme.border_focus							= color_yellow
+theme.border_marked							= color_yellow
+theme.titlebar_bg_focus						= color_yellow
+theme.titlebar_bg_normal					= color_white
+theme.taglist_fg_focus						= color_yellow
+theme.tasklist_bg_focus						= color_black
+theme.tasklist_fg_focus						= color_yellow
+theme.textbox_widget_margin_top			= 1
+theme.notify_fg								= theme.fg_normal
+theme.notify_bg								= theme.bg_normal
+theme.notify_border							= theme.border_focus
+theme.awful_widget_height					= 14
+theme.awful_widget_margin_top				= 10
+theme.mouse_finder_color					= color_yellow
+theme.menu_height								= "16"
+theme.menu_width								= "140"
+
+-- Icons
+theme.submenu_icon							= themes_dir .. "/icons/submenu.png"
+theme.taglist_squares_sel					= themes_dir .. "/icons/square_sel.png"
+theme.taglist_squares_unsel				= themes_dir .. "/icons/square_unsel.png"
+
+theme.layout_tile								= themes_dir .. "/icons/tile.png"
+theme.layout_tilegaps						= themes_dir .. "/icons/tilegaps.png"
+theme.layout_tileleft						= themes_dir .. "/icons/tileleft.png"
+theme.layout_tilebottom						= themes_dir .. "/icons/tilebottom.png"
+theme.layout_tiletop							= themes_dir .. "/icons/tiletop.png"
+theme.layout_fairv							= themes_dir .. "/icons/fairv.png"
+theme.layout_fairh							= themes_dir .. "/icons/fairh.png"
+theme.layout_spiral							= themes_dir .. "/icons/spiral.png"
+theme.layout_dwindle							= themes_dir .. "/icons/dwindle.png"
+theme.layout_max								= themes_dir .. "/icons/max.png"
+theme.layout_fullscreen						= themes_dir .. "/icons/fullscreen.png"
+theme.layout_magnifier						= themes_dir .. "/icons/magnifier.png"
+theme.layout_floating						= themes_dir .. "/icons/floating.png"
+
+theme.widget_ac								= themes_dir .. "/icons/ac.png"
+theme.widget_battery							= themes_dir .. "/icons/battery.png"
+theme.widget_battery_low					= themes_dir .. "/icons/battery_low.png"
+theme.widget_battery_empty					= themes_dir .. "/icons/battery_empty.png"
+theme.widget_mem								= themes_dir .. "/icons/mem.png"
+theme.widget_cpu								= themes_dir .. "/icons/cpu.png"
+theme.widget_temp								= themes_dir .. "/icons/temp.png"
+theme.widget_net								= themes_dir .. "/icons/net.png"
+theme.widget_hdd								= themes_dir .. "/icons/hdd.png"
+theme.widget_music							= themes_dir .. "/icons/note.png"
+theme.widget_music_on						= themes_dir .. "/icons/note_on.png"
+theme.widget_vol								= themes_dir .. "/icons/vol.png"
+theme.widget_vol_low							= themes_dir .. "/icons/vol_low.png"
+theme.widget_vol_no							= themes_dir .. "/icons/vol_no.png"
+theme.widget_vol_mute						= themes_dir .. "/icons/vol_mute.png"
+theme.widget_mail								= themes_dir .. "/icons/mail.png"
+theme.widget_mail_on							= themes_dir .. "/icons/mail_on.png"
+
+theme.tasklist_disable_icon				= false
+theme.tasklist_floating						= ""
+theme.tasklist_maximized_horizontal		= ""
+theme.tasklist_maximized_vertical		= ""
+
+return theme
+
